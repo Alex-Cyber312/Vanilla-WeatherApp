a01c276fc6554260ea1ac3a07b506eabf01d9d6b
Alex <69380217+Alex-Cyber312@users.noreply.github.com>
2022-12-03 03:55:33 -0600

    api

--- Src/app.js
+++ Src/app.js
@@ -1,1 +1,12 @@
-alert('Hello World');
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector('#temperature');
+  let cityElement = document.querySelector('#city');
+  let descriptionElement = document.querySelector('#description');
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = '99ae64dd2f14be64782ee23c1487d267';
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
