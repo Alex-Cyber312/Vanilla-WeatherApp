25659b73ff279c18fc8158faa7b09f19551c4ef9
Alex <69380217+Alex-Cyber312@users.noreply.github.com>
2022-12-06 23:20:57 -0600

    showing current temperature

--- Src/app.js
+++ Src/app.js
@@ -1,12 +1,19 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector('#temperature');
   let cityElement = document.querySelector('#city');
   let descriptionElement = document.querySelector('#description');
+  let humidityElement = document.querySelector('#humidity');
+  let windElement = document.querySelector('#wind');
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
+let city = 'New York';
 let apiKey = '99ae64dd2f14be64782ee23c1487d267';
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 axios.get(apiUrl).then(displayTemperature);
