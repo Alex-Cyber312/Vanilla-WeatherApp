aa252eb0ffceed88bf41c5e0d9a8b1322a03eb28
Alex <69380217+Alex-Cyber312@users.noreply.github.com>
2022-12-10 02:11:01 -0600

    Added search engine

--- Src/app.js
+++ Src/app.js
@@ -43,8 +43,18 @@ function displayTemperature(response) {
   iconElement.setAttribute('alt', response.data.weather[0].description);
 }
 
-let city = 'New York';
-let apiKey = '99ae64dd2f14be64782ee23c1487d267';
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = '99ae64dd2f14be64782ee23c1487d267';
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector('#city-input');
+  search(cityInputElement.value);
+}
+search('New York');
+
+let form = document.querySelector('#search-form');
+form.addEventListener('submit', handleSubmit);
